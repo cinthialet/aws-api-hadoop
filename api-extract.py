b5e3339713d50e6b5c4c5839f2267dad64753027
cinthialet <cithsantos@gmail.com>
2024-01-14 22:36:06 -0300

    fix lambda

--- api-extract.py
+++ api-extract.py
@@ -1,74 +1,52 @@
-#########
-# Script da Lambda
-#########
-
 import json
 import boto3
 import requests
-import time
 from datetime import datetime, timedelta
 import os
 
 def lambda_handler(_event, _context):
     """
-    Realiza a coleta de dados de uma API financeira (Alpha Vantage) a cada 2 minutos, por um total de 10 minutos,
-    e salva os resultados em arquivos JSON no Amazon S3.
+    Coleta dados da ação especificada da API Alpha Vantage para cada dia útil da semana anterior. 
+    A função é acionada segunda-feira às 9 AM UTC e realiza uma única requisição, compilando dados para 
+    cada dia útil de semanas anteriores em um arquivo JSON.
 
-    As variáveis de ambiente são utilizadas para definir os parâmetros da API e o nome do bucket do S3.
-    A função registra cada ação no CloudWatch para monitoramento.
-
-    Args:
-        event: Objeto de evento que disparou a função Lambda (não utilizado neste script).
-        context: Objeto de contexto fornecido pela AWS Lambda (não utilizado neste script).
-
-    Raises:
-        HTTPError: Erro se a API retornar um código de status HTTP não bem-sucedido.
-        RequestException: Erro para problemas gerais de requisição (rede, etc.).
+    O script coleta dados dos últimos 5 dias úteis da semana anterior (de segunda a sexta-feira da semana anterior).
+    Os dados são salvos em um folder 'raw' no S3, especificado pela variável de ambiente BUCKET_LAYER.
     """
-    
-    # Carrega as variáveis de ambiente da Lambda
+
     API_KEY = os.getenv('API_KEY')
-    FUNCTION = os.getenv('FUNCTION')
-    SYMBOL = os.getenv('SYMBOL')
-    INTERVAL = os.getenv('INTERVAL')
+    SYMBOL = os.getenv('SYMBOL') 
+    FUNCTION = os.getenv('FUNCTION') 
     BUCKET_NAME = os.getenv('BUCKET_NAME')
+    BUCKET_LAYER = os.getenv('BUCKET_LAYER')  # Nome do folder para salvar os dados, raw
 
-    # URL da API Alpha Vantage com os parâmetros definidos
-    url = f'https://www.alphavantage.co/query?function={FUNCTION}&symbol={SYMBOL}&interval={INTERVAL}&apikey={API_KEY}'
-
-    # Cliente S3
     s3_client = boto3.client('s3')
 
-    # Nome da pasta baseado na data de execução do script
-    folder_name = f"api-extract-{datetime.now().strftime('%Y%m%d')}"
+    # Dicionário para armazenar os dados dos dias úteis
+    weekly_data = {}
 
-    # Calculando o tempo de término para daqui a 10 minutos
-    end_time = datetime.now() + timedelta(minutes=10)
+    # Itera sobre os últimos 5 dias úteis (evitando fim de semana)
+    for i in range(3, 8):  # 3 a 7 incluído
+        # Calcula a data para a requisição (dia útil da semana anterior)
+        date_for_request = (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')
 
-    while datetime.now() < end_time:
-        try:
-            # Faz a chamada para a API para pegar os dados
-            response = requests.get(url)
-            response.raise_for_status()  # Levanta um erro se o status não for 200
+        # Monta a URL da requisição para a API Alpha Vantage
+        url = f'https://www.alphavantage.co/query?function={FUNCTION}&symbol={SYMBOL}&apikey={API_KEY}&datatype=json'
 
-            # Gera um nome de arquivo com o timestamp atual até registro de minuto
-            file_name = f"{folder_name}/{datetime.now().strftime('%Y%m%d-%H%M')}-api-response.json"
+        # Faz a requisição para a API Alpha Vantage
+        response = requests.get(url)
+        data = response.json()
 
-            # Salva a resposta em um arquivo JSON no S3 - put_object
-            s3_client.put_object(Body=json.dumps(response.json()), Bucket=BUCKET_NAME, Key=file_name)
+        # Armazena os dados coletados no dicionário, com a chave sendo a data
+        weekly_data[date_for_request] = data
 
-            print(f"Resposta salva no S3: {file_name}")
+    # Define o nome do arquivo que será salvo no S3
+    file_name = f"{BUCKET_LAYER}/weekly-data-api-response.json"
 
-        except requests.HTTPError as e:
-            # Erro específico para códigos de status HTTP ruins
-            print(f"Erro ao acessar a API: Status Code {response.status_code}")
+    # Salva todos os dados coletados no arquivo JSON no S3
+    s3_client.put_object(Body=json.dumps(weekly_data), Bucket=BUCKET_NAME, Key=file_name)
 
-        except requests.RequestException as e:
-            # Outros erros de requisição
-            print(f"Ocorreu um erro ao fazer a chamada à API: {e}")
+    # Registra no log o arquivo salvo
+    print(f"Resposta salva no S3: {file_name}")
 
-        # Espera por 2 minutos antes da próxima iteração
-        time.sleep(120)
-
-    # Print para monitoramento no cloudwatch
-    print(f"Coleta e armazenamento de dados finalizados com sucesso. Os arquivos foram salvos em {BUCKET_NAME}/{folder_name}")
+    print(f"Coleta e armazenamento de dados finalizados com sucesso. Os dados da semana foram salvos em {BUCKET_NAME}/{BUCKET_LAYER}")
